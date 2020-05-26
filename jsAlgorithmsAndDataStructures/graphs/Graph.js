ee1d3c9bcf971a7e9332d4226f9250581ce302ec
Adrian Meza <adrian.meza@parivedasolutions.com>
2020-05-26 01:10:09 -0500

    add graph DFSrecursive graph traversal method (@adrianme213)

--- jsAlgorithmsAndDataStructures/graphs/Graph.js
+++ jsAlgorithmsAndDataStructures/graphs/Graph.js
@@ -27,13 +27,40 @@ class Graph {
 
     delete this.adjacencyList[v1];
   }
+
+  DFSrecursive(vertex, results=[], visitedVertices={}) {
+    if(!vertex) return null;
+
+    results.push(vertex);
+    visitedVertices[vertex] = true;
+    this.adjacencyList[vertex].forEach((key, val, set) => {
+      if(!visitedVertices[key]){
+        this.DFSrecursive(val, results, visitedVertices);
+      }
+    });
+
+    return results;
+  }
+
+  DFSiterative(vertex) {
+
+  }
 }
 
-// var g = new Graph();
-// g.addVertex("LA");
-// g.addVertex("SF");
-// g.addVertex("NYC");
-// g.addEdge("LA", "SF");
-// g.addEdge("SF", "NYC");
-//
-// g.adjacencyList;
+var g = new Graph();
+g.addVertex("A");
+g.addVertex("B");
+g.addVertex("C");
+g.addVertex("D");
+g.addVertex("E");
+g.addVertex("F");
+
+g.addEdge("A", "B");
+g.addEdge("A", "C");
+g.addEdge("B", "D");
+g.addEdge("C", "E");
+g.addEdge("D", "E");
+g.addEdge("D", "F");
+g.addEdge("E", "F");
+
+g.DFSrecursive("A");
