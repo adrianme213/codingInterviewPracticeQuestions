cf711567991ba3988704bee0b0a2f8f13b425560
Adrian Meza <adrian.meza@parivedasolutions.com>
2020-10-07 23:57:24 -0700

    add Oct 7 leetcode problem (@adrianme213)

---
+++ leetcodeProblems/javascript/rotateLinkedList.js
@@ -0,0 +1,68 @@
+/*
+Given a linked list, rotate the list to the right by k places, where k is non-negative.
+
+Example 1:
+
+Input: 1->2->3->4->5->NULL, k = 2
+Output: 4->5->1->2->3->NULL
+Explanation:
+rotate 1 steps to the right: 5->1->2->3->4->NULL
+rotate 2 steps to the right: 4->5->1->2->3->NULL
+
+Example 2:
+
+Input: 0->1->2->NULL, k = 4
+Output: 2->0->1->NULL
+Explanation:
+rotate 1 steps to the right: 2->0->1->NULL
+rotate 2 steps to the right: 1->2->0->NULL
+rotate 3 steps to the right: 0->1->2->NULL
+rotate 4 steps to the right: 2->0->1->NULL
+
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+var rotateRight = function(head, k) {
+    if(k === 0 || head === null) return head;
+
+    let len = 1;
+    let curr = head;
+    while(curr !== null) {
+        curr = curr.next;
+        if(curr !== null) len++;
+    }
+
+    if(len === 1 || k%len === 0) return head;
+
+    let kTwin = len - (k % len);
+    curr = head;
+    let prev = null;
+    while(kTwin > 0) {
+        prev = curr;
+        curr = curr.next;
+        kTwin--;
+    }
+
+
+    let oldHead = head;
+    head = curr;
+    prev.next = null;
+    let connectPieces = head;
+    prev = null;
+    while(connectPieces !== null) {
+        prev = connectPieces;
+        connectPieces = connectPieces.next;
+        if(connectPieces === null) {
+            prev.next = oldHead;
+        }
+    }
+
+    return head;
+};
