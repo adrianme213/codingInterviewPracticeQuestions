ae709bd1f6e5a08aa357404c093256835ce6eb3f
Adrian Meza <adrian.meza@parivedasolutions.com>
2020-10-28 09:38:17 -0700

    add Oct 27-28 leetcode problems (@adrianme213)

---
+++ leetcodeProblems/javascript/summaryRanges.js
@@ -0,0 +1,93 @@
+/*
+You are given a sorted unique integer array nums.
+
+Return the smallest sorted list of ranges that cover all the numbers in the array exactly. That is, each element of nums is covered by exactly one of the ranges, and there is no integer x such that x is in one of the ranges but not in nums.
+
+Each range [a,b] in the list should be output as:
+
+    "a->b" if a != b
+    "a" if a == b
+
+
+
+Example 1:
+
+Input: nums = [0,1,2,4,5,7]
+Output: ["0->2","4->5","7"]
+Explanation: The ranges are:
+[0,2] --> "0->2"
+[4,5] --> "4->5"
+[7,7] --> "7"
+
+Example 2:
+
+Input: nums = [0,2,3,4,6,8,9]
+Output: ["0","2->4","6","8->9"]
+Explanation: The ranges are:
+[0,0] --> "0"
+[2,4] --> "2->4"
+[6,6] --> "6"
+[8,9] --> "8->9"
+
+Example 3:
+
+Input: nums = []
+Output: []
+
+Example 4:
+
+Input: nums = [-1]
+Output: ["-1"]
+
+Example 5:
+
+Input: nums = [0]
+Output: ["0"]
+
+
+
+Constraints:
+
+    0 <= nums.length <= 20
+    -231 <= nums[i] <= 231 - 1
+    All the values of nums are unique.
+*/
+/**
+ * @param {number[]} nums
+ * @return {string[]}
+ */
+var summaryRanges = function(nums) {
+    if(nums.length === 0) return [];
+    if(nums.length === 1) return [String(nums[0])];
+    const ranges = [];
+    let start = 0;
+    let follow = 0;
+    let end = 1;
+    while(end < nums.length) {
+        if(nums[end]-nums[follow] === 1) {
+            end++;
+            follow++;
+        } else if (nums[end]-nums[follow] > 1) {
+            if(nums[follow] === nums[start]) {
+                ranges.push(`${nums[start]}`);
+                start = end;
+                follow = end;
+                end += 1;
+            } else {
+                ranges.push(`${nums[start]}->${nums[follow]}`);
+                start = end;
+                follow = end;
+                end += 1;
+            }
+        }
+    }
+
+    // Add final range
+    if(nums[start] === nums[follow]) {
+        ranges.push(`${nums[start]}`);
+    } else {
+        ranges.push(`${nums[start]}->${nums[follow]}`);
+    }
+
+    return ranges;
+};
