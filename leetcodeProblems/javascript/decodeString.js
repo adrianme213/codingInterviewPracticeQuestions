57723ccf239a2a3d0f9c5049158d1e11afb7a072
Adrian Meza <adrian.meza@parivedasolutions.com>
2020-11-19 08:39:09 -0800

    add Nov 19 leetcode problem (@adrianme213)

---
+++ leetcodeProblems/javascript/decodeString.js
@@ -0,0 +1,59 @@
+/*
+Given an encoded string, return its decoded string.
+
+The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is being repeated exactly k times. Note that k is guaranteed to be a positive integer.
+
+You may assume that the input string is always valid; No extra white spaces, square brackets are well-formed, etc.
+
+Furthermore, you may assume that the original data does not contain any digits and that digits are only for those repeat numbers, k. For example, there won't be input like 3a or 2[4].
+
+Example 1:
+
+Input: s = "3[a]2[bc]"
+Output: "aaabcbc"
+
+Example 2:
+
+Input: s = "3[a2[c]]"
+Output: "accaccacc"
+
+Example 3:
+
+Input: s = "2[abc]3[cd]ef"
+Output: "abcabccdcdcdef"
+
+Example 4:
+
+Input: s = "abc3[cd]xyz"
+Output: "abccdcdcdxyz"
+
+Constraints:
+
+    1 <= s.length <= 30
+    s consists of lowercase English letters, digits, and square brackets '[]'.
+    s is guaranteed to be a valid input.
+    All the integers in s are in the range [1, 300].
+
+*/
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var decodeString = function(s) {
+    let str = s.slice();
+
+    while(str.includes('[')) {
+        const startBracketIdx = str.lastIndexOf('[');
+        let numberStartIdx = startBracketIdx-1;
+        let regex = /[0-9]/;
+        while(regex.test(str[numberStartIdx]) && numberStartIdx >= 0) {
+            numberStartIdx--;
+        }
+        numberStartIdx++;
+        const closeEndIdx = str.indexOf(']', numberStartIdx);
+        const num = Number(str.substring(numberStartIdx, startBracketIdx));
+        const innerStr = str.substring(startBracketIdx+1, closeEndIdx);
+        str = str.substring(0, numberStartIdx) + innerStr.repeat(num) + str.substring(closeEndIdx+1);
+    }
+    return str;
+};
