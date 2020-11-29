745ce44b29f27b9525efa5262ce11e8b1e7ff8e2
Adrian Meza <adrian.meza@parivedasolutions.com>
2020-11-29 11:45:09 -0800

    add Nov 29 leetcode problem (@adrianme213)

---
+++ leetcodeProblems/javascript/jumpGameThree.js
@@ -0,0 +1,54 @@
+/*
+Given an array of non-negative integers arr, you are initially positioned at start index of the array. When you are at index i, you can jump to i + arr[i] or i - arr[i], check if you can reach to any index with value 0.
+
+Notice that you can not jump outside of the array at any time.
+Example 1:
+Input: arr = [4,2,3,0,3,1,2], start = 5
+Output: true
+Explanation: 
+All possible ways to reach at index 3 with value 0 are: 
+index 5 -> index 4 -> index 1 -> index 3 
+index 5 -> index 6 -> index 4 -> index 1 -> index 3 
+
+Example 2:
+Input: arr = [4,2,3,0,3,1,2], start = 0
+Output: true 
+Explanation: 
+One possible way to reach at index 3 with value 0 is: 
+index 0 -> index 4 -> index 1 -> index 3
+
+Example 3:
+Input: arr = [3,0,2,1,2], start = 2
+Output: false
+Explanation: There is no way to reach at index 1 with value 0.
+
+Constraints:
+
+    1 <= arr.length <= 5 * 10^4
+    0 <= arr[i] < arr.length
+    0 <= start < arr.length
+
+*/
+/**
+ * @param {number[]} arr
+ * @param {number} start
+ * @return {boolean}
+ */
+var canReach = function(arr, start, memo={}) {
+    if(arr[start] === 0) return true;
+    
+    let leftStart = start - arr[start];
+    if(leftStart >= 0 && memo[leftStart] === undefined) {
+        memo[leftStart] = 'visited';
+        const leftTrue = canReach(arr, leftStart, memo);
+        if(leftTrue) return true;
+    }
+    let rightStart = start + arr[start];
+    if(rightStart < arr.length && memo[rightStart] === undefined) {
+        memo[rightStart] = 'visited';
+        const rightTrue = canReach(arr, rightStart, memo);
+        if(rightTrue) return true;
+    } 
+    
+    return false;
+};
